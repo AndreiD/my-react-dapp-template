cc4545750b7f1f04c0b526f558d7f9397b93169b
Andrei <danandreist@gmail.com>
2019-02-23 13:09:01 +0200

    :zap:

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,91 +3,96 @@ import Web3 from 'web3'
 import UnlockMetamask from './metamask/UnlockMetamask'
 import InstallMetamask from './metamask/InstallMetamask'
 
+let web3 = window.web3;
 
 class Home extends Component {
   constructor() {
     super();
-
     this.isWeb3 = true; //If metamask is installed
     this.isWeb3Locked = false; //If metamask account is locked
 
     this.state = {
-      network: 'Checking...',
+      networkName: 'Checking...',
       account: null,  //address of the currently unlocked metamask
     }
 
-    let web3 = window.web3;
 
-    //set web3 & truffle contract
+
     if (typeof web3 !== 'undefined') {
-      // Use Mist/MetaMask's provider
       this.web3Provider = web3.currentProvider;
       this.web3 = new Web3(web3.currentProvider);
-      if (web3.eth.coinbase === null) this.isWeb3Locked = true;
+
+      // this.tokenZendr = TruffleContract(TokenZendR);
+      //this.tokenZendr.setProvider(this.web3Provider);
+
+      if (web3.eth.coinbase === null) {
+        this.isWeb3Locked = true;
+      }
 
     } else {
       this.isWeb3 = false;
     }
 
-
   }
-
-
-  //switch statement to check the current network and set the
-  //value to be displayed on the nav component
-  setNetwork = () => {
-    let networkName = this;
-
-    this.web3.eth.net.getId(function (err, networkId) {
-      switch (networkId) {
-        case "1":
-          networkName = "Main";
-          break;
-        case "2":
-          networkName = "Morden";
-          break;
-        case "3":
-          networkName = "Ropsten";
-          break;
-        case "4":
-          networkName = "Rinkeby";
-          break;
-        case "42":
-          networkName = "Kovan";
-          break;
-        default:
-          networkName = networkId;
-      }
-
-      this.setState({
-        network: networkName
-      })
-    });
-  };
-
 
   componentDidMount() {
-    let account = this.web3.eth.coinbase;
-    this.setNetwork();
-    this.setState({
-      account
-    });
+    this.isWeb3Locked = true;
+    web3.currentProvider.publicConfigStore.on('update', this.metamaskUpdateCallback);
   }
+
+  metamaskUpdateCallback = ({ selectedAddress, networkVersion }) => {
+
+    this.isWeb3 = true;
+    this.isWeb3Locked = false;
+
+    let networkName, that = this;
+    console.log()
+    switch (networkVersion) {
+      case "1":
+        networkName = "Main";
+        break;
+      case "2":
+        networkName = "Morden";
+        break;
+      case "3":
+        networkName = "Ropsten";
+        break;
+      case "4":
+        networkName = "Rinkeby";
+        break;
+      case "42":
+        networkName = "Kovan";
+        break;
+      default:
+        networkName = networkVersion;
+    }
+
+    that.setState({ networkName: networkName, account: selectedAddress })
+
+  }
+
+
 
 
   render() {
+
     if (this.isWeb3) {
       if (this.isWeb3Locked) {
         return (
-          <div>
-            ...
-            <UnlockMetamask message="Please Unlock Your Metamask/Mist Wallet" />
-          </div>
+          <UnlockMetamask message="Please Unlock Your Metamask/Mist Wallet" />
         )
       } else {
         return (
-          <div>
-            connected ok! {this.state.network}
+          <div className="row">
+            <div className="col s6 offset-s3">
+              <div className="card-panel blue-grey darken-1">
+                <div className="card-content white-text">
+                  <span className="card-title">Connected to: {this.state.networkName}</span>
+                  {this.state.account && <p>Account: {this.state.account}</p>}
+                </div>
+
+              </div>
+            </div>
           </div>
         )
       }
@@ -97,8 +102,6 @@ class Home extends Component {
       )
     }
   }
-
-
 
 
 }
